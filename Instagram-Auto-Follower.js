042af2cf1953faea654f766de5700e25c8b12613
lyricsjoker <70332391+lyricsjoker@users.noreply.github.com>
2020-10-02 11:32:15 +0530

    Update Instagram-Auto-Follower.js

--- Instagram-Auto-Follower.js
+++ Instagram-Auto-Follower.js
@@ -1,42 +1,14 @@
-const timeoutPromise = (timeout) => new Promise((resolve) => setTimeout(resolve, timeout));
-var list = document.querySelectorAll('.L3NKy');
-
-function clikOnLink(link) {
-    if (link.firstChild.nodeValue == "Follow") {
-        link.click();
-    }
+(() => {
+let i = 5;
+const followInterval = setInterval(() => {
+if (i >= 50) {
+clearInterval(followInterval);
+return;
 }
-
-function ScrollJS(selector) {
-    if(selector == "suggested"){
-        var current = document.documentElement.scrollTop;
-        scrollTo(current, current+300);
-    }
-    else if(selector == "othersfollowers"){
-        var fDialog = document.querySelector('div[role="dialog"] .isgrP');
-        fDialog.scrollTop = fDialog.scrollTop + 300;
-    }
-}
-
-async function FollowPeople(start, end,selector) {
-    for (i = start; i < end; i++) {
-        clikOnLink(list[i]);
-        console.log("Person: "+i);
-        await timeoutPromise(10000);
-    }
-    ScrollJS(selector);
-}
-
-async function FunctionMain(selector) {
-
-    for (i = 0; i <= list.length; i + 5) {
-        if(i<=list.length-1){
-            list = document.querySelectorAll('.L3NKy');
-        }
-        FollowPeople(i, i + 5,selector);
-        await timeoutPromise(80000);
-    }
-
-}
-
-FunctionMain("suggested");
+const buttons = document.querySelectorAll('button');
+const nextButton = buttons[i];
+nextButton.click();
+i ++;
+}, 2000);
+console.log("intervalID => ",followInterval);
+})();
